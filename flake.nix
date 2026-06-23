9d99d14bee401ff0aab45c95c7849bca0149e034
Josiah Berkebile <praenato14@gmail.com>
2026-06-23 17:34:09 +0000

    Compose Mercury and Bats dev shell

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   inputs = {
     mise.url = "git+https://github.com/josiah14/mise.git";
+    nixpkgs.follows = "mise/nixpkgs";
   };
 
-  outputs = { self, mise }:
+  outputs = { self, mise, nixpkgs }:
     let
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      devShells.${system}.default = mise.devShells.${system}.mercury-22-01-8;
+      devShells.${system}.default = pkgs.mkShell {
+        packages =
+          mise.lib.${system}.mercury-22-01-8
+          ++ mise.lib.${system}.bats-1-12-0;
+      };
     };
 }
